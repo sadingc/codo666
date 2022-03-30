014b090baf52bdbd50ffd52739c4ae932144a5aa
antoj15 <99308145+antoj15@users.noreply.github.com>
2022-03-30 11:15:20 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 BOT_TOKEN = os.environ.get('5233802174:AAFOIYKfx4g0dKUH7hiZga0vdIiWenW6Ggg','')
 TG_API_ID = os.environ.get('14586634',0)
 TG_API_HASH = os.environ.get('76be0d3cbc21ba384b593a012f6ee9c0','')
-TG_ADMIN_USER = os.environ.get('toni8790','')
+TG_ADMIN_USER = os.environ.get('hunter4345','tigerpu')
 ROOT_DIR = 'root'
